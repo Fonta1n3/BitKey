7ee2e6630b899fb301a3a93fd4bc7bd5dd3451d0
TripKey <tripkeyapp@gmail.com>
2018-03-21 20:13:11 +1100

    Updating Mayer Multiple.

--- BitKeys/MayerMultipleViewController.swift
+++ BitKeys/MayerMultipleViewController.swift
@@ -24,6 +24,8 @@ class MayerMultipleViewController: UIViewController {
         self.button.setTitle("Back", for: .normal)
         self.button.addTarget(self, action: #selector(self.goBack), for: .touchUpInside)
         self.view.addSubview(self.button)
+        
+        getMayerMultiple()
     }
 
     @objc func goBack() {
@@ -32,14 +34,127 @@ class MayerMultipleViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getMayerMultiple() {
+        
+        var url:NSURL!
+        url = NSURL(string: "https://blockchain.info/charts/market-price?timespan=200days&format=json")
+        
+        let task = URLSession.shared.dataTask(with: url! as URL) { (data, response, error) -> Void in
+            
+            do {
+                
+                if error != nil {
+                    
+                    print(error as Any)
+                    
+                    
+                } else {
+                    
+                    if let urlContent = data {
+                        
+                        do {
+                            
+                            let jsonQuoteResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableLeaves) as! NSDictionary
+                            
+                            if let prices = jsonQuoteResult["values"] as? NSArray {
+                                
+                                var priceArray = [Int]()
+                                
+                                for price in prices {
+                                    
+                                    let daysPrice = (price as! NSDictionary)["y"] as! Double
+                                    priceArray.append(Int(daysPrice))
+                                    
+                                }
+                                
+                                let twoHundredDayMovingAverage = priceArray.average
+                                
+                                var url:NSURL!
+                                url = NSURL(string: "https://api.coindesk.com/v1/bpi/currentprice.json")
+                                
+                                let task = URLSession.shared.dataTask(with: url! as URL) { (data, response, error) -> Void in
+                                    
+                                    do {
+                                        
+                                        if error != nil {
+                                            
+                                            print(error as Any)
+                                            
+                                            
+                                        } else {
+                                            
+                                            if let urlContent = data {
+                                                
+                                                do {
+                                                    
+                                                    let jsonQuoteResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableLeaves) as! NSDictionary
+                                                    
+                                                    if let exchangeCheck = jsonQuoteResult["bpi"] as? NSDictionary {
+                                                        
+                                                        if let usdCheck = exchangeCheck["USD"] as? NSDictionary {
+                                                            
+                                                            if let rateCheck = usdCheck["rate_float"] as? Float {
+                                                                
+                                                                DispatchQueue.main.async {
+                                                                    
+                                                                    let exchangeRate = Double(rateCheck)
+                                                                    print("exchangeRate = \(exchangeRate)")
+                                                                    
+                                                                    let mayerMultipleLabel = UITextView()
+                                                                    mayerMultipleLabel.frame = CGRect(x: self.view.frame.minX + 15, y: self.view.center.y - (self.view.frame.height / 3), width: self.view.frame.width - 30, height: 300)
+                                                                    mayerMultipleLabel.text = "The Bitcoin Mayer Multiple is \(round(100 * (twoHundredDayMovingAverage / exchangeRate) / 100)) with a current price of $\(round(100 * exchangeRate) / 100) USD and a 200 day moving average of $\(round(100 * twoHundredDayMovingAverage) / 100) USD."
+                                                                    mayerMultipleLabel.textColor = UIColor.black
+                                                                    mayerMultipleLabel.font = UIFont.systemFont(ofSize: 32)
+                                                                    mayerMultipleLabel.textAlignment = .natural
+                                                                    self.view.addSubview(mayerMultipleLabel)
+                                                                    
+                                                                }
+                                                                
+                                                            }
+                                                            
+                                                        }
+                                                        
+                                                    }
+                                                    
+                                                } catch {
+                                                    
+                                                    print("JSon processing failed")
+                                                }
+                                            }
+                                            
+                                        }
+                                    }
+                                }
+                                
+                                task.resume()
+                                
+                                
+                            }
+                            
+                        } catch {
+                            
+                            print("JSon processing failed")
+                            
+                        }
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+        
     }
-    */
 
+}
+
+extension Array where Element == Int {
+    
+    var total: Element {
+        return reduce(0, +)
+    }
+    
+    var average: Double {
+        return isEmpty ? 0 : Double(reduce(0, +)) / Double(count)
+    }
+    
 }
